ac0253828b5da60ed2dd14dfffd17d7d2c72f69a
chittalasvdinesh <chittalasvdinesh12@gmail.com>
2024-04-29 16:51:46 +0530

    changes due to build issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <h2 className="text-4xl">Welcome to  <span className="text-blue-500">Employee Tracker!!!</span></h2>
-        <p className="text-2xl py-5">Employee Tracker is a powerful tool designed to help you manage your employee data efficiently. Whether you're a small business owner, a human resources manager, or simply looking to streamline your employee management process, Employee Tracker has you covered.</p>
+        <p className="text-2xl py-5">Employee Tracker is a powerful tool designed to help you manage your employee data efficiently. Whether you are a small business owner, a human resources manager, or simply looking to streamline your employee management process, Employee Tracker has you covered.</p>
         <p className="text-2xl">With Employee Tracker, you can easily view, add, and edit employee information, all from one convenient dashboard. Keep track of essential details such as employee names, contact information, job titles, departments, and more. Say goodbye to messy spreadsheets and manual data entry – Employee Tracker simplifies the process of managing employee data, saving you time and effort.
         </p>
         <div className="flex justify-center items-center mt-5">
